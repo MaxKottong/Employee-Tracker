60cc2328b0c205798d5a450b1895c9613b8ffb1b
Max Kottong <max.kottong@gmail.com>
2022-08-20 14:47:45 -0400

    Added connections to other documents on server.js

--- Server.js
+++ Server.js
@@ -1,5 +1,8 @@
 const db = require('./utils/Database');
-const express = require('express');
 const PORT = process.env.PORT || 3001;
-const app = express();
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const Employee = require('./lib/Employee');
+const Department = require('./lib/Department');
+const Role = require('./lib/Role');
 
