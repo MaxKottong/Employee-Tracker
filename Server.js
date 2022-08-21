d4a9b303b4794b06b6735375c70d306dd9f37750
Max Kottong <max.kottong@gmail.com>
2022-08-21 12:26:38 -0400

    Added function comments for setup

--- Server.js
+++ Server.js
@@ -6,3 +6,23 @@ const Employee = require('./lib/Employee');
 const Department = require('./lib/Department');
 const Role = require('./lib/Role');
 
+//Start function -- Asks what you want to do, then takes the data from the response, creates a switch for each selection,
+//and each selection starts its own function.
+
+//addDepartment function
+
+//addRole function
+
+//addEmployee function
+
+//updateEmployeeRole function
+
+//updateEmployeeManager function
+
+//removeEmployee function
+
+//removeRole function
+
+//remove department function
+
+//call start function (each separate function will call start at the end so you can return to the menu)
